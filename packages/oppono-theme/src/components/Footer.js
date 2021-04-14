1264c9c1c5520621ab2346152ba88d29c87de723
ModyydoM <modyydom@gmail.com>
2021-04-14 14:32:05 +0200

    Anything that is linking to Contacts slider should now link to the mail page

--- packages/oppono-theme/src/components/Footer.js
+++ packages/oppono-theme/src/components/Footer.js
@@ -68,7 +68,7 @@ const Footer = React.forwardRef(({className}, forwardRef) => {
           <FooterRight className={'footer-right'}>
             <Link className={'primary'} href="/terms/">Terms And Conditions</Link>
             <Link className={'primary'} href="/privacy-policy/">Privacy Policy</Link>
-            <Link className={'primary'} href="/contacts/">Contact us</Link>
+            <Link className={'primary'} href="/mail/">Contact us</Link>
             <Link className={'primary'} href="tel:19058865352">Call us</Link>
           </FooterRight>
         </div>
