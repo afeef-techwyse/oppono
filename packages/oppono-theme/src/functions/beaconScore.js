ad6d2ae6b99d15488f0a8dc5ee8b59d15e96ecd0
ModyydoM <modyydom@gmail.com>
2021-05-14 03:09:34 +0200

    fix beaconScore display

--- packages/oppono-theme/src/functions/beaconScore.js
+++ packages/oppono-theme/src/functions/beaconScore.js
@@ -1,3 +1,10 @@
-  export function beaconScore(beacon_score) {
-    return beacon_score?.[0].split('-')[0]+(beacon_score?.length>1?'+':'');
+export function beaconScore(beacon_score) {
+  if (beacon_score?.length>1) {
+    console.log(beacon_score?.length,beacon_score);
+    const firstPart = beacon_score?.[0].split('-')[0];
+    const lastPart = beacon_score?.[beacon_score?.length-1].split('-')[1];
+    
+    return firstPart + (lastPart?'-'+lastPart:'+')
   }
+  return beacon_score?.[0]
+}
